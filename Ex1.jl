78d447454ead3db49897929231212004ea7cc259
Lorenzo Ribeiro Varalo <80349379+LorenzoVaralo@users.noreply.github.com>
2022-03-22 20:50:16 -0300

    Update do path

--- Ex1.jl
+++ Ex1.jl
@@ -5,7 +5,7 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ b4ad831c-a8e1-11ec-2dc2-8ffb74e0342d
-include("/home/lorenzovaralo/Documentos/ProjetosPython/Project_Euler/Check.jl")
+include("$(@__DIR__)/Check.jl")
 
 # ╔═╡ 6ea7bdd0-9778-4b29-91b5-a8f2db418a28
 resposta = sum([x for x in 0:999 if x % 3 == 0 || x % 5 == 0 ])
